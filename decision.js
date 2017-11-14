a11138190088f718424a1d1a3d63357514472307
morozovnikita <nimokirotazov@gmail.com>
2017-11-14 16:58:09 +0000

    Update decision.js

--- decision.js
+++ decision.js
@@ -1,12 +1,14 @@
 
 function rand(min, max){
-	return (max-min)*Math.random()+min; //Высчитываем рандомную задержку от 0 до 1с
+	return (max-min)*Math.random()+min; 
 }
 
 function f(a){
   for (var i = 0 ; i < 10 ; i++){
     console.log(a*i);
   }
+  setTimeout(()=>{
+    f(2);
+  }, rand(1000 , 5000))
 }
-
-setInterval(f(2) , rand(0 , 1000)); // Выставляем интервал
+f(2);
